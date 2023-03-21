60b3a2d2a97251aa663fb4a6c4ff5b2f0fac67cf
Rex77proIV <smaurieka2@gmail.com>
2023-03-21 14:29:45 +0100

    Added notes

--- Client.py
+++ Client.py
@@ -2,6 +2,7 @@ from socket import *
 import requests
 
 # Calling REST Api - sending data
+# Get files from API and convert to json
 api_URL = "https://p2psharingapi20230321122351.azurewebsites.net/api/FileEndpoints"
 response = requests.get(api_URL)
 data = response.json()
@@ -9,13 +10,16 @@ print(data)
 
 chooseFile = input("Please Write File Name: ")
 
+# Extract fileName, ip & host
 response = requests.get(api_URL + '/' + chooseFile)
 serverPeerData = response.json()
 print(serverPeerData)
 peerData = serverPeerData[0]
 
+# Write filename
 fileName = peerData["fileName"]
 
+# Connect to Peer Server
 serverName = peerData["ip"]
 serverPort = peerData["port"]
 clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -26,6 +30,7 @@ dataToSend = peerData["fileName"] + ";"
 serverPeerData = clientSocket.send(dataToSend.encode())
 
 # Receive Data from Server Peer
+# Set filePath to put file gotten
 file = open('c:/temp/' + fileName, 'wb')
 file_data = clientSocket.recv(1024)
 while file_data:
@@ -33,10 +38,3 @@ while file_data:
     file.write(file_data)
     file_data = clientSocket.recv(1024)
 file.close()
-
-# Hent filer fra API, og lav om til json -> Din fil indeholder filNavn, ip og host ->
-# Brug ip og Host til at connect til Server -> send fil navn til Server ->
-
-
-
-
